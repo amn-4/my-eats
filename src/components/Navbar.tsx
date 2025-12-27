f99f7a959b32681be6f52d3c0ee57cd8028dfaeb
amn-4 <amenah.sab@gmail.com>
2025-12-27 22:47:39 +1100

    feat: add button to create restaurants in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,10 @@
 
 "use client"
 
-import { AppBar, Toolbar, Typography } from "@mui/material"
+import { AppBar, Toolbar, Typography, Tooltip, IconButton } from "@mui/material"
 import ThemeToggle from "@/components/ThemeToggle"
+import { Add } from "@mui/icons-material"
+import Link from "next/link"
 
 export default function Navbar() {
   return (
@@ -12,6 +14,20 @@ export default function Navbar() {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
         melb.eats
         </Typography>
+
+        {/* add restaurant button */}
+        <Tooltip title="Add Restaurant">
+          <IconButton
+            component={Link}
+            href="/add"
+            color="inherit"
+            sx={{ mr: 2 }}
+            aria-label="add restaurant"
+          >
+            <Add />
+          </IconButton>
+        </Tooltip>
+
         <ThemeToggle />
     </Toolbar>
     </AppBar>
