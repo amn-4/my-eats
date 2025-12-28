96bd4e9a337d4bbf55150cf0e3a5aa77a6d0364c
amn-4 <amenah.sab@gmail.com>
2025-12-28 13:07:55 +1100

    feat: improve navbar and restaurant card layout
    add link to homepage on melb.eats title with sliding underline animation
    
    position edit button next to restaurant name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 "use client"
 
-import { AppBar, Toolbar, Typography, Tooltip, IconButton } from "@mui/material"
+import { AppBar, Toolbar, Typography, Tooltip, IconButton, Box } from "@mui/material"
 import ThemeToggle from "@/components/ThemeToggle"
 import { Add } from "@mui/icons-material"
 import Link from "next/link"
@@ -11,9 +11,36 @@ export default function Navbar() {
   return (
     <AppBar position="sticky">
     <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        melb.eats
-        </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography 
+            variant="h6" 
+            component={Link}
+            href="/"
+            sx={{ 
+              textDecoration: "none",
+              color: "inherit",
+              cursor: "pointer",
+              position: "relative",
+              display: "inline-block",
+              transition: "all 0.3s ease",
+              "&::after": {
+                content: '""',
+                position: "absolute",
+                bottom: -2,
+                left: 0,
+                width: 0,
+                height: 2,
+                backgroundColor: "currentColor",
+                transition: "width 0.5s ease",
+              },
+              "&:hover::after": {
+                width: "100%",
+              }
+            }}
+          >
+            melb.eats
+          </Typography>
+        </Box>
 
         {/* add restaurant button */}
         <Tooltip title="Add Restaurant">
